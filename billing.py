12735ece5bfd776e0e5c0c40f1651b88993fc9ab
Elliot Morgan <elliotmrgn@gmail.com>
2023-04-01 20:58:00 -0400

    starter code

---
+++ billing.py
@@ -0,0 +1,27 @@
+# ----------------------------------------------------------------
+# Author:
+# Date:
+#
+# This module calculates and displays billing information
+# for students in the class registration system.  Student and
+# class records are reviewed and tuition fees are calculated.
+# -----------------------------------------------------------------
+def calculate_hours_and_bill(id, s_in_state, c_rosters, c_hours):
+    # ------------------------------------------------------------
+    # This function calculate billing information. It takes four
+    # parameters: id, the student id; s_in_state, the list of
+    # in-state students; c_rosters, the rosters of students in
+    # each course; c_hours, the number of hours in each course.
+    # This function returns the number of course hours and tuition
+    # cost.
+    # ------------------------------------------------------------
+    pass  # temporarily avoid empty function definition
+
+
+def display_hours_and_bill(hours, cost):
+    # ------------------------------------------------------------
+    # This function prints the number of course hours the student
+    # is taking and the total tuition cost. It takes two parameters:
+    # hours and cost. This function has no return value.
+    # ------------------------------------------------------------
+    pass  # temporarily avoid empty function definition
